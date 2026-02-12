f40ff2033780ecb828e270c1d2e7213e0716d810
icey12221 <alyzt14@nottingham.ac.uk>
2026-02-12 23:22:04 +0000

    Add missing source files

--- worksheet5/adder.h
+++ worksheet5/adder.h
@@ -1,11 +1,22 @@
-/** @file
- *  @brief Exported maths library functions.
- */
+#pragma once
 
-/**
- * @brief Adds two integers.
- * @param a First integer
- * @param b Second integer
- * @return Sum of a and b
- */
-int add(int a, int b);
+// 1) Static build: no dll import/export decorations
+#ifdef maths_STATIC
+  #define MATHSLIB_API
+#else
+  // 2) Shared build (DLL)
+  #if defined(_WIN32) || defined(__CYGWIN__)
+    #ifdef maths_EXPORTS
+      // building the DLL
+      #define MATHSLIB_API __declspec(dllexport)
+    #else
+      // using the DLL
+      #define MATHSLIB_API __declspec(dllimport)
+    #endif
+  #else
+    #define MATHSLIB_API
+  #endif
+#endif
+
+// Function declaration
+extern "C" MATHSLIB_API int add(int a, int b);
