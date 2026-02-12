e3fc82702ade38baaf1838f564330e8436deee0d
icey12221 <alyzt14@nottingham.ac.uk>
2026-02-12 23:18:40 +0000

    Add Worksheet5 sources and Doxyfile

---
+++ worksheet5/adder.h
@@ -0,0 +1,11 @@
+/** @file
+ *  @brief Exported maths library functions.
+ */
+
+/**
+ * @brief Adds two integers.
+ * @param a First integer
+ * @param b Second integer
+ * @return Sum of a and b
+ */
+int add(int a, int b);
